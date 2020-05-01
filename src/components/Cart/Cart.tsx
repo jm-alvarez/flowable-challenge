8f0820ac68f27b22fb90a848b1abd4b1560b35fb
José Miguel Álvarez Vañó <jose-miguel.alvarez@gft.com>
2020-05-01 10:11:17 +0200

    Convert Cart to functional component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import CartProducts from '../../model/Cart';
-import Product from '../../model/Product';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateProductStock } from '../../services/ProductService';
 import { emptyCart } from '../../state/actions';
 import { GlobalState } from '../../state/reducers';
@@ -11,85 +9,53 @@ import Checkout from '../Checkout/Checkout';
 import './Cart.scss';
 
 interface IProps {
-  cart: CartProducts;
-  products: Product[];
   showProductList: () => void;
-  onEmptyCart: () => void;
 }
 
-interface IState {
-  checkingOut: boolean;
-}
+const Cart = (props: IProps) => {
+  const [checkingOut, setCheckingOut] = useState(false);
+  const { cart, products } = useSelector((state: GlobalState) => state);
+  const dispatch = useDispatch();
 
-class Cart extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
+  const doCheckout = () => {
+    setCheckingOut(true);
 
-    this.state = {
-      checkingOut: false,
-    };
-  }
-
-  render() {
-    return (
-      <div className="cart-container">
-        <div className="title">
-          <i
-            className="material-icons"
-            title="Go to the product list"
-            onClick={this.props.showProductList}
-          >
-            arrow_back_ios
-          </i>
-          <h2>Cart</h2>
-        </div>
-        {Object.keys(this.props.cart).length > 0 ? (
-          <React.Fragment>
-            <CartList />
-            <Checkout doCheckout={this.doCheckout} checkingOut={this.state.checkingOut} />
-          </React.Fragment>
-        ) : (
-          <div className="cart-empty-message">Cart is empty</div>
-        )}
-      </div>
-    );
-  }
-
-  getProduct = (id: string) => this.props.products.filter((product) => product.id === id)[0];
-
-  doCheckout = () => {
-    this.setState({
-      checkingOut: true,
-    });
-
-    this.props.products.forEach((product) => {
-      if (this.props.cart[product.id]) {
+    products.forEach((product) => {
+      if (cart[product.id]) {
         updateProductStock(product.id, product.stock);
       }
     });
 
     setTimeout(() => {
       emitCheckoutCompletedToast();
-      this.props.onEmptyCart();
-      this.setState({
-        checkingOut: false,
-      });
-      this.props.showProductList();
+      dispatch(emptyCart());
+      setCheckingOut(false);
+      props.showProductList();
     }, 1000);
   };
-}
 
-const mapStateToProps = (state: GlobalState) => {
-  return {
-    cart: state.cart,
-    products: state.products,
-  };
+  return (
+    <div className="cart-container">
+      <div className="title">
+        <i
+          className="material-icons"
+          title="Go to the product list"
+          onClick={props.showProductList}
+        >
+          arrow_back_ios
+        </i>
+        <h2>Cart</h2>
+      </div>
+      {Object.keys(cart).length > 0 ? (
+        <React.Fragment>
+          <CartList />
+          <Checkout doCheckout={doCheckout} checkingOut={checkingOut} />
+        </React.Fragment>
+      ) : (
+        <div className="cart-empty-message">Cart is empty</div>
+      )}
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    onEmptyCart: () => dispatch(emptyCart()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cart);
+export default Cart;
